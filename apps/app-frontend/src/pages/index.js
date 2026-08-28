d5ce7ba8140e02e9ccda496b2d4520502234ac74
MASIK <ivanmaspanov24@gmail.com>
2026-08-28 16:32:05 +0300

    Delete apps directory

--- apps/app-frontend/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-import Index from './Index.vue'
-import Browse from './Browse.vue'
-
-export { Index, Browse }
--- apps/app-frontend/src/pages/instance/index.js
+++
@@ -1,5 +0,0 @@
-import Index from './Index.vue'
-import Mods from './Mods.vue'
-import Logs from './Logs.vue'
-
-export { Index, Mods, Logs }
